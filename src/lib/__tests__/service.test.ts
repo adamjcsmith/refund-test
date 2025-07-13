8288d2f3b1b5f4211070b5c15a6b959e1d1e350a
Adam Smith <adamjcsmith@gmail.com>
2025-07-13 14:13:46 +0100

    Basic LUT for converting TZs to IANA names, plus test

--- src/lib/__tests__/service.test.ts
+++ src/lib/__tests__/service.test.ts
@@ -2,6 +2,23 @@ import { determineRefundEligibility, isNewTOS } from '../service';
 import { ReversalRequest } from '@/types';
 
 describe('isNewTOS', () => {
+
+  it('Unsupported timezones should be rejected', () => {
+    const mockRequest: ReversalRequest = {
+      name: 'Emma Smith',
+      customerTZ: 'Unknown',
+      signupDate: '1/2/2020',
+      source: 'phone',
+      investmentDate: '1/2/2021',
+      investmentTime: '06:00',
+      requestDate: '1/2/2021',
+      requestTime: '09:00'
+    };
+    
+    const result = isNewTOS(mockRequest);
+    expect(result).toEqual([new Error('Unknown timezone: Unknown'), false]);
+  });
+
   it('New TOS (US-based)', () => {
     const mockRequest: ReversalRequest = {
       name: 'Emma Smith',
--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,13 +1,28 @@
 import { ErrorFirstTuple, ReversalRequest } from "@/types"
 
+// Using a LUT here to map the customerTZ to the correct timezone.
+// This is a simple lookup table and will need to be updated if we add more timezones,
+// or better yet use an IANA timezone name, like below:
+const TZ_LOOKUP = {
+  'US (PST)': 'America/Los_Angeles',
+  'US (EST)': 'America/New_York',
+  'Europe (CET)': 'Europe/Paris',
+  'Europe (GMT)': 'Europe/London',
+}
+
 export const isNewTOS = (request: ReversalRequest): ErrorFirstTuple<boolean> => {
-  // TODO: Implement actual logic to determine if the request is from the new TOS
-  console.log('Checking if request is from the new TOS:', request.name);
+  // If the user has signed up before 2/1/2020 (UK) or 1/2/2020 (US) then they are on the old TOS
+  // Otherwise, they are on the new TOS. We can use the customerTZ to determine the correct date.
+
+  const IANA_TZ = TZ_LOOKUP[request.customerTZ as keyof typeof TZ_LOOKUP];
+  if (!IANA_TZ) {
+    return [new Error(`Unknown timezone: ${request.customerTZ}`), false];
+  }
+
   return [new Error("Not implemented"), true]
 }
 
 export const determineRefundEligibility = (request: ReversalRequest): boolean => {
-  // TODO: Implement actual refund eligibility logic
   console.log('Checking eligibility for request:', request.name);
   return false
 }
